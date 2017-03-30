0e93c6e5da105e42bac13cdd683acf0894911088
Yulson <iuliia.baranova@gmail.com>
2017-03-30 21:58:13 +0300

    Changed Task1 for the Lesson4, created 2 methods that generate array and put it into console

--- 4. Operators-and-Constructors/Operators-and-constructors-task1.rb
+++ 4. Operators-and-Constructors/Operators-and-constructors-task1.rb
@@ -6,51 +6,55 @@
 #in case we've got "0" we should puts "we've got 0"
 #in each case we should get 2 messages except the '0' case
 
+class Method1
 
+	def initialize
+		@array_of_integers = []
+		i = 0 
+		while i < 10
+		  	num = rand(9) 
+		  	@array_of_integers.push(num)
+		  	i = i + 1
+		end
+	end
 
-array_of_integers = []
-i = 0 
-while i < 10
-  num = rand(9) 
-  array_of_integers.push(num)
-  i = i + 1
+	def print_array
+		puts @array_of_integers
+		@array_of_integers.each do |num|
+			if  num == 0
+				puts 'we have got 0'
+				puts '=========='
+
+			elsif num == 5
+				puts 'the number is odd'
+				puts 'the number is 5'
+				puts '=========='
+
+			elsif num.odd? && num <5
+				puts 'the number is odd'
+				puts 'the number is less than 5'
+				puts '=========='
+
+			elsif num.odd? && num >5
+				puts 'the number is odd'
+				puts 'the number is more than 5'
+				puts '=========='
+					
+			elsif num.even? && num <5
+				puts 'the number is even'
+				puts 'the number is less than 5'
+				puts '=========='
+
+			else num.even? && num >5
+				puts 'the number is even'
+				puts 'the number is more than 5'
+				puts '=========='	
+			end
+		end
+	end
+
+	array = Method1.new
+	array.print_array
 end
 
-puts array_of_integers
-array_of_integers.each do |num|
 
-if  num == 0
-	puts 'we have got 0'
-	puts '=========='
-
-elsif num == 5
-	puts 'the number is odd'
-	puts 'the number is 5'
-	puts '=========='
-
-elsif num.odd? && num <5
-	puts 'the number is odd'
-	puts 'the number is less than 5'
-	puts '=========='
-
-elsif num.odd? && num >5
-	puts 'the number is odd'
-	puts 'the number is more than 5'
-	puts '=========='
-		
-elsif num.even? && num <5
-	puts 'the number is even'
-	puts 'the number is less than 5'
-	puts '=========='
-
-else num.even? && num >5
-	puts 'the number is even'
-	puts 'the number is more than 5'
-	puts '=========='	
-end
-end
-
-
-
-	
-		
